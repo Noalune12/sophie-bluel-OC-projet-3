0b14765a9901fd7d32b5c8d023ed75f528f99235
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-03-04 12:14:55 +0100

    Génération des projets depuis l'API

--- FrontEnd/assets/script/projets.js
+++ FrontEnd/assets/script/projets.js
@@ -1,7 +1,32 @@
+//Etape 1.1
+
 
 const sectionProjets = document.querySelector(".gallery");
+
+const response = await fetch('http://localhost:5678/api/works');
+let projet = await response.json();
 
 //Reset de la galerie de projets
 function resetProjets() {
     sectionProjets.innerHTML = "";
 }
+
+resetProjets();
+
+//Génération des projets depuis l'API
+function genererProjets(projet) {
+    for(let i = 0; i < projet.length; i++) {
+        const figure = document.createElement("figure");
+        sectionProjets.appendChild(figure);
+        const img = document.createElement("img");
+        img.src = projet[i].imageUrl;
+        img.alt = projet[i].title;
+        figure.appendChild(img);
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = projet[i].title;
+        figure.appendChild(figcaption);
+    }
+
+}
+
+genererProjets(projet)
