49a7e5acb161019a32e3a90492e8b1c2771a9886
Lou-Anne Buisson <louanne.buisson@gmail.com>
2024-03-04 16:17:28 +0100

    Ajout des filtres projets

--- FrontEnd/assets/script/projets.js
+++ FrontEnd/assets/script/projets.js
@@ -30,3 +30,63 @@ function genererProjets(projet) {
 }
 
 genererProjets(projet)
+
+//Etape 1.2 - Trier les projets par catégorie
+const btncontainer = document.querySelector(".filters");
+const btnFilter = document.querySelectorAll(".btnFilter");
+
+const btnTous = document.querySelector(".btnTous");
+
+btnTous.addEventListener("click", function(){
+    const projetsFiltres = projet
+    resetProjets();
+    genererProjets(projetsFiltres);
+});
+
+const btnObjets = document.querySelector(".btnObjets");
+
+btnObjets.addEventListener("click", function(){
+    const projetsFiltres = projet.filter(function(projet) {
+        return projet.categoryId === 1;
+    });
+    resetProjets();
+    genererProjets(projetsFiltres);
+});
+
+const btnAppart = document.querySelector(".btnAppart");
+
+btnAppart.addEventListener("click", function(){
+    const projetsFiltres = projet.filter(function(projet) {
+        return projet.categoryId === 2;
+    });
+    resetProjets();
+    genererProjets(projetsFiltres);
+});
+
+const btnHotels = document.querySelector(".btnHotels");
+
+btnHotels.addEventListener("click", function(){
+    const projetsFiltres = projet.filter(function(projet) {
+        return projet.categoryId === 3;
+    });
+    resetProjets();
+    genererProjets(projetsFiltres);
+});
+
+for (var index = 0; index < btnFilter.length; index++) {
+    btnFilter[index].addEventListener("click", function(){
+        var current = document.getElementsByClassName("selected");
+        current[0].className = current[0].className.replace(" selected", "");
+        this.className += " selected";
+    });
+}
+
+
+/*btnFilter.forEach(bouton => {
+    bouton.addEventListener("click", function() {
+        bouton.classList.remove('selected');
+    });
+    this.classList.add('selected');
+});*/
+
+
